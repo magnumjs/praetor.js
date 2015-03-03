54da8907e0c9f4a8a05e66e12f5aec098ec7aecf
magnumjs <magnumjs@users.noreply.github.com>
2015-03-03 09:44:05 -0500

    Added j.proc tests

--- tests/praetorjs-api-spec.js
+++ tests/praetorjs-api-spec.js
@@ -6,14 +6,101 @@ describe("Praetor", function() {
 
   describe('p() unique id instances', function() {
     it("can set & get a unique instance of datastore", function() {
-      p.setDataStore('test', {}, 'id')
-      expect(p.getDataStore('test', 'id')).toEqual({});
-      expect(p.getDataStore('test', 'id2')).toEqual(undefined);
-      p.setDataStore('test2', {}, 'id2')
-      expect(p.getDataStore('test2', 'id2')).toEqual({});
-      expect(p.getDataStore('test2', 'id')).toEqual(undefined);
+      expect(p('id').setDataStore('test', {}).getDataStore('test')).toEqual({});
+      expect(p('id2').getDataStore('test')).toEqual(undefined);
+      expect(p('id2').setDataStore('test2', {}).getDataStore('test2')).toEqual({});
+      expect(p('id').getDataStore('test2')).toEqual(undefined);
     });
-  });
+    it("can set & get a unique instance of state", function() {
+      expect(p('id', {
+        stores: {
+          books: []
+        }
+      }).getState()).toEqual({
+        stores: {
+          books: []
+        },
+        queries: {},
+        procs: {}
+      });
+      expect(p('id2', {
+        stores: {
+          movies: []
+        }
+      }).getState()).toEqual({
+        stores: {
+          movies: []
+        },
+        queries: {},
+        procs: {}
+      });
+      expect(p('id2', {
+        stores: {
+          comics: []
+        }
+      }).getState()).toEqual({
+        stores: {
+          movies: [],
+          comics: []
+        },
+        queries: {},
+        procs: {}
+      });
+      expect(p('id', {
+        queries: {
+          'titles': {
+            query: '$..title',
+            store: 'books'
+          }
+        }
+      }).getState()).toEqual({
+        stores: {
+          books: []
+        },
+        queries: {
+          titles: {
+            query: '$..title',
+            store: 'books'
+          }
+        },
+        procs: {}
+      });
+      expect(p.getState()).toEqual({
+        stores: {
+          books: {
+            books: [{
+              title: 'kids',
+              author: 'adams'
+            }, {
+              title: 'action',
+              author: 'johns'
+            }]
+          }
+        },
+        queries: {},
+        procs: {}
+      });
+    })
+  })
+
+  describe('p.proc', function() {
+    it('can take json and single jsonpath queriy to render a result json', function() {
+      expect(p.proc(books, ['$..title'])).toEqual([
+        ['kids', 'action']
+      ]);
+    })
+    it('can take json and multiple jsonpath queries to render a result json', function() {
+      expect(p.proc(books, ['$..title', '$..author'])).toEqual([
+        ['kids', 'action'],
+        ['adams', 'johns']
+      ]);
+    })
+    it('can take json and single jsonpath queriy to render a result json with code', function() {
+      expect(p.proc(books, ['$..title'], 'this.results[0].reverse()')).toEqual([
+        ['action', 'kids']
+      ]);
+    })
+  })
 
   it("can set & get a data store", function() {
     expect(p.getDataStore('books')).toEqual(books);
@@ -39,6 +126,7 @@ describe("Praetor", function() {
     });
   });
 
+
   it("can set & get a stored procedure with return", function() {
 
     var code = '\
@@ -61,6 +149,7 @@ describe("Praetor", function() {
     expect(results[0]['getBookTitles']).toEqual(["KIDS", "ACTION"]);
   });
 
+
   it("can set & get a stored procedure with NO return", function() {
 
     var code = '\
@@ -78,6 +167,8 @@ describe("Praetor", function() {
     var results = p.getStoredProcResult('convertBookTitles', {
       upperCase: true
     });
+
     expect(results[0]['getBookTitles']).toEqual(["KIDS", "ACTION"]);
   });
+
 });
