53e6e1ba78be5d2cfac9ff22f6973a6e723462e3
magnumjs <magnumjs@users.noreply.github.com>
2015-03-04 04:41:11 -0500

    Update boot.js

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -1,39 +1,22 @@
 /**
- *  Use aysnc script loader, configure the application module (for AngularJS)
- *  and initialize the application ( which configures routing )
+ *  
  *
- *  @author Thomas Burleson
+ *  @author Michael Glazer
  */
 
- (function( window, head ) {
+ (function( window, undefinef ) {
     "use strict";
 
-    head.js(
+   var app ={}
 
-      // Pre-load these for splash-screen progress bar...
+app.controller=function (){
 
-      { require    : "./vendor/requirejs/require.js",                  size: "80196"   },
-      { underscore : "./vendor/underscore/underscore.js",              size: "43568"   },
+}
+app.view=function (){
+ return m('.container','hello world!')
+}
 
-      { angular    : "./vendor/angular/angular.js",                    size: "551057"  },
-      { ngRoute    : "./vendor/angular-route/angular-route.js",        size: "30052"   },
-      { ngSanitize : "./vendor/angular-sanitize/angular-sanitize.js",  size: "19990"   },
-
-      { quizzler   : "./assets/js/quizzler.js"                                         }
-
-    )
-    .ready("ALL", function()
-    {
-        // All application code is concat/uglified in 1 file:  `quizzler.js`
-
-        require( [ "main" ], function( app )
-        {
-            // Application has bootstrapped and started...
-        });
+m.module(document.getElementById('praetor-ui'), app)
 
 
-    });
-
-
-
-}( window, head ));
+}( window, undefined ));
