be92306e90870e82864b2dc022982e5b66a57910
mglazer <michael.glazer@contractor.cengage.com>
2015-03-06 08:10:35 -0500

    more tests

---
+++ tests/proc-spec.js
@@ -0,0 +1,27 @@
+if (typeof require !== 'undefined') {
+    p = require("praetor")
+}
+
+describe('p.proc()', function () {
+
+    var booksString = '{"books":[{"title":"kids", "author":"adams"},{"title":"action", "author":"johns"}]}';
+    var books = JSON.parse(booksString);
+
+    it('can take json and single jsonpath queriy to render a result json', function () {
+        expect(p.proc(books, ['$..title'])).toEqual([
+                                                        ['kids', 'action']
+                                                    ]);
+    })
+    it('can take json and multiple jsonpath queries to render a result json', function () {
+        expect(p.proc(books, ['$..title', '$..author'])).toEqual([
+                                                                     ['kids', 'action'],
+                                                                     ['adams', 'johns']
+                                                                 ]);
+    })
+    it('can take json and single jsonpath queriy to render a result json with code', function () {
+        expect(p.proc(books, ['$..title'], 'this.results[0].reverse()')).toEqual([
+                                                                                     ['action',
+                                                                                      'kids']
+                                                                                 ]);
+    })
+})
