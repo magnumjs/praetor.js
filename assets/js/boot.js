c5cc96792f1c7886d2d8e02b4b46d1427d0cba05
magnumjs <magnumjs@users.noreply.github.com>
2015-03-05 21:00:16 -0500

    Update boot.js

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -13,7 +13,10 @@ app.controller=function (){
 
 }
 app.view=function (){
- return m('.content','hello world!')
+ return m('.content','This will be an admin UI editor for the Praetor.JS API and the two core usage methods, p() & p.proc()',
+ 'Praetor is a tier library to allow for altering JSON resultsets somewhere in between fetching the JSON and rendering it.',
+ 'a simple example: ','p.proc (getMovies (),[ \'$..title\' ],\'this.results[0].reverse ()\')')
+)
 }
 
 m.module(document.getElementsByClassName('content-area')[0], app)
