621e33ffd47136744fe08406761046f3d9830d48
mglazer <michael.glazer@contractor.cengage.com>
2015-03-09 13:58:36 -0400

    refactored some code for readability

--- praetor.js
+++ praetor.js
@@ -14,8 +14,9 @@
 
 var p = (function (undefined) {
     'use strict';
-    var rootid = 'rootid'
 
+
+    // Query Engine Interface Endpoint
     // TODO: make this pluggable to any query engine
     // DefiantJS, JSONPath, JSONQuery, JPath etc...
     function getjsonpath() {
@@ -25,7 +26,9 @@ var p = (function (undefined) {
         return JSONPath
     }
 
-    // UTIL FUNCTIONS
+    // BEGIN UTIL FUNCTIONS
+    // TODO: why two mege functions, diff, nested issues?
+    // explain functional diff between these two
     var realMerge = function (to, from) {
         for (var n in from) {
             if (typeof to[n] != 'object') {
@@ -42,18 +45,6 @@ var p = (function (undefined) {
         return to;
     };
 
-    function clone(a) {
-        return JSON.parse(JSON.stringify(a));
-    }
-
-    // TODO: why two mege functions, diff, nested issues?
-    //function shallowCopy(to, from) {
-    //    for (var attr in from) {
-    //        to[attr] = to[attr] === undefined ? from[attr] : to[attr]
-    //    }
-    //    return to
-    //}
-
     function copy(to, from) {
         for (var attr in from) {
             if (typeof to[attr] != 'object' ) {
@@ -69,56 +60,22 @@ var p = (function (undefined) {
         return to
     }
 
-    // public cache access for tests?
-    // across all state IDs?
-    var funCache={};
-    function createFunctionExec(queryResults, storedProc, parms) {
-
-        // create unique has id
-        var uid = JSONPath.hashCode(JSON.stringify({proc: storedProc, parms : parms}))
-        funCache[uid] = funCache[uid] = {}
-
-        if(!funCache[uid].fun) {
-            // create temp function
-            funCache[uid].fun = Function(storedProc.code)
-        }
-        parms = parms || {}
-        //merge parms with defaults
-        //TODO: parms will override if attr don't exist
-        // This is NOT a merge
-        for (var attr in storedProc.parms) {
-            parms[attr] = parms[attr] === undefined ? storedProc.parms[attr] : parms[attr]
-        }
-        //TODO: optional context via defaults override options
-        var context = {
-            results: queryResults,
-            params: parms
-        }
-
-        // execute proc and return results
-        var ret = funCache[uid].fun.apply(context)
-        if (ret) {
-            return ret
-        }
-        return context.results
+    function clone(a) {
+        return JSON.parse(JSON.stringify(a));
     }
+    // END UTIL FUNCTIONS
 
-    // UTIL FUNCTIONS
+    var OBJECT = "[object Object]",NULL = "[object Null]", ARRAY = "[object Array]", STRING = "[object String]", FUNCTION = "function"
+    var type = {}.toString
 
-    //TODO: is map necessary?
-    // since we now have p.model?
-    // data contruct
-    //var map = {}
-    //map.stores = {}
-    //map.queries = {}
-    //map.procs = {}
+    // default 'global' state ID
+    var rootid = 'rootid'
 
-    // copy - don't modify!
-    //var model = function () {
-    //    return JSON.parse(JSON.stringify(map))
-    //}()
+    var defaults = {jsonPathOptions: {}}, // library defaults
+        options = {}, // user supplied
+        settings = {} // combo of defaults and over rides in options
 
-    //return wireframe
+    //return wireframe - not modifiable
     p.model = function () {
         return {
             stores: {},
@@ -127,12 +84,6 @@ var p = (function (undefined) {
         }
     }
 
-    var defaults = {jsonPathOptions: {}}, // library defaults
-        options = {}, // user supplied
-        settings = {} // combo of defaults and over rides in options
-
-    var OBJECT = "[object Object]",NULL = "[object Null]", ARRAY = "[object Array]", STRING = "[object String]", FUNCTION = "function"
-    var type = {}.toString
 
     // initializer
     function p(state, id, options) {
@@ -176,18 +127,87 @@ var p = (function (undefined) {
         return this.state[rootid]
     }
 
-    // TODO: remove is this necessary seems redundant to p.proc
-    // setting a named proc
-    //p.procWith = function (name, storeName, queries, code, parms) {
-    //    // if 1 argument run it
-    //    if (arguments.length == 1) {
-    //        return p.getStoredProcResult(name, id)
-    //    } else {
-    //        p.setJsonQuery(name, queries, storeName, id)
-    //        p.setStoredProc(name, name, code, parms, id)
-    //    }
-    //}
+    // API METHODS - step order
 
+    // overwrite existing leaves (stores, procs, queries)
+    p.setState = function (state, id) {
+        var nstate = copy(state || {}, p.model())
+        p.map(id, nstate)
+    }
+    p.getState = function (id) {
+        //return a copy not a reference
+        return clone(p.map(id));
+    }
+
+    // API DATA STORE
+
+    p.setDataStore = function (name, data, id) {
+        p.map(id).stores[name] = data
+    }
+
+    p.getDataStore = function (name, id) {
+        return p.map(id).stores[name]
+    }
+
+    p.removeDataStore = function (name, id) {
+        delete p.map(id).stores[name]
+    }
+
+    p.emptyDataStore = function (name, id) {
+        p.map(id).stores[name] = {}
+    }
+
+    // API QUERIES
+
+    p.setJsonQuery = function (name, JsonPathQuery, storeName, id) {
+        p.map(id).queries[name] = {
+            query: JsonPathQuery,
+            store: storeName
+        }
+    }
+    p.getJsonQuery = function (name, id) {
+        return p.map(id).queries[name]
+    }
+    p.getJsonQueryResult = function (name, parms, id) {
+        var jsonQuery = p.getJsonQuery(name, id)
+        var data = p.getDataStore(jsonQuery.store, id)
+
+        // interpolate the query with the parms
+        for(var k in parms){
+            jsonQuery.query=jsonQuery.query.split(k).join(parms[k])
+        }
+        var result = getjsonpath()(p.settings().jsonPathOptions, data, jsonQuery.query)
+        return result
+    }
+
+    // API PROCEDURES
+
+    p.setStoredProc = function (name, namedQueries, codeBody, parms, id) {
+        p.map(id).procs[name] = {
+            queries: type.call(namedQueries) == ARRAY ? namedQueries : namedQueries.split(','), // must be an array if comma separated then split
+            code: codeBody,
+            parms: parms
+        }
+    }
+    p.getStoredProc = function (name, id) {
+        return p.map(id).procs[name]
+    }
+
+    p.getStoredProcResult = function (name, parms, id) {
+        var storedProc = p.getStoredProc(name, id)
+        var queryResults = [];
+        // get all results
+        queryResults = storedProc.queries.map(function (val, idx) {
+            var result = {}
+            result[val] = p.getJsonQueryResult(val, parms, id)
+            return result
+        })
+
+        return createFunctionExec(queryResults, storedProc, parms);
+    }
+
+    // Polymorphic function can have name or null
+    // can have named queries or not
     // TODO: refactor into above method with this function name
     p.proc = function (name, json, queries, code, parms, id) {
         // if 1 argument run it
@@ -224,75 +244,40 @@ var p = (function (undefined) {
         return createFunctionExec(queryResults, storedProc, parms);
     }
 
-    p.setDataStore = function (name, data, id) {
-        p.map(id).stores[name] = data
-    }
+    // Reusable core function
 
-    p.getDataStore = function (name, id) {
-        return p.map(id).stores[name]
-    }
+    // public cache access for tests?
+    // across all state IDs?
+    var funCache={};
+    function createFunctionExec(queryResults, storedProc, parms) {
 
-    p.removeDataStore = function (name, id) {
-        delete p.map(id).stores[name]
-    }
+        // create unique has id
+        var uid = getjsonpath().hashCode(JSON.stringify({proc: storedProc, parms : parms}))
+        funCache[uid] = funCache[uid] = {}
 
-    p.emptyDataStore = function (name, id) {
-        p.map(id).stores[name] = {}
-    }
-
-    p.setJsonQuery = function (name, JsonPathQuery, storeName, id) {
-        p.map(id).queries[name] = {
-            query: JsonPathQuery,
-            store: storeName
+        if(!funCache[uid].fun) {
+            // create temp function
+            funCache[uid].fun = Function(storedProc.code)
         }
-    }
-    p.getJsonQuery = function (name, id) {
-        return p.map(id).queries[name]
-    }
-    p.getJsonQueryResult = function (name, parms, id) {
-        var jsonQuery = p.getJsonQuery(name, id)
-        var data = p.getDataStore(jsonQuery.store, id)
-
-        // interpolate the query with the parms
-        for(var k in parms){
-            jsonQuery.query=jsonQuery.query.split(k).join(parms[k])
+        parms = parms || {}
+        //merge parms with defaults
+        //TODO: parms will override if attr don't exist
+        // This is NOT a merge
+        for (var attr in storedProc.parms) {
+            parms[attr] = parms[attr] === undefined ? storedProc.parms[attr] : parms[attr]
         }
-        var result = getjsonpath()(p.settings().jsonPathOptions, data, jsonQuery.query)
-        return result
-    }
-
-    p.setStoredProc = function (name, namedQueries, codeBody, parms, id) {
-        p.map(id).procs[name] = {
-            queries: type.call(namedQueries) == ARRAY ? namedQueries : namedQueries.split(','), // must be an array if comma separated then split
-            code: codeBody,
-            parms: parms
+        //TODO: optional context via defaults override options
+        var context = {
+            results: queryResults,
+            params: parms
         }
-    }
-    p.getStoredProc = function (name, id) {
-        return p.map(id).procs[name]
-    }
 
-    p.getStoredProcResult = function (name, parms, id) {
-        var storedProc = p.getStoredProc(name, id)
-        var queryResults = [];
-        // get all results
-        queryResults = storedProc.queries.map(function (val, idx) {
-            var result = {}
-            result[val] = p.getJsonQueryResult(val, parms, id)
-            return result
-        })
-
-        return createFunctionExec(queryResults, storedProc, parms);
-    }
-
-    // overwrite existing leaves (stores, procs, queries)
-    p.setState = function (state, id) {
-        var nstate = copy(state || {}, p.model())
-        p.map(id, nstate)
-    }
-    p.getState = function (id) {
-        //return a copy not a reference
-        return clone(p.map(id));
+        // execute proc and return results
+        var ret = funCache[uid].fun.apply(context)
+        if (ret) {
+            return ret
+        }
+        return context.results
     }
 
     return p
