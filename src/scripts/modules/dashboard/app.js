3ab5650c05341e888a2e96bbf5e178449dd97f3f
mglazer <michael.glazer@contractor.cengage.com>
2015-03-11 16:29:50 -0400

    auto completed with movies in dash as component

--- src/scripts/modules/dashboard/app.js
+++ src/scripts/modules/dashboard/app.js
@@ -2,6 +2,8 @@ m = require('mithril');
 p = require("praetor")
 persist = require("../../persistence.js")
 utils = require("../../utils.js")
+autocompleter = require("../../components/autocompleter")
+
 
 // dashboard module
 var dashboard = function () {
@@ -39,10 +41,9 @@ var dashboard = function () {
            // this.projects = m.prop([{id: 1, name: "John's project"}, {id: 2, name: "Bob's project"}, {id: 2, name: "Mary's project"}]);
 
             this.projects = movies.listEven().then(function (users) {
-                //if (users.length == 0) m.route("/add");
 
                 // id, title, url, small_cover_image
-                
+
                // this.projects = m.prop([{id: 1, name: "John's project"}, {id: 2, name: "Bob's project"}, {id: 2, name: "Mary's project"}]);
                 return users
 
@@ -59,8 +60,9 @@ var dashboard = function () {
              m('span.card-color', m('h1.cards.card-color-title', 'New Movies')),
              m('.card-content', [
                  m('p.cards', 'Search newest: ',
-                   m('input[placeholder="Filter by Genre"]')),
-                 ctrl.projectAC.view({field:'title',data: ctrl.projects, binds: ctrl.selectedProject}),
+                   //m('input[placeholder="Filter by Genre"]')
+                   ctrl.projectAC.view({field:'title',data: ctrl.projects, binds: ctrl.selectedProject})
+                 ),
                  m('h4.cards', 'See run time code')
              ])
             ])
@@ -71,28 +73,3 @@ var dashboard = function () {
 
 module.exports = dashboard
 
-var autocompleter = function () {
-    var autocompleter = {}
-    autocompleter.vm = {
-        term: m.prop(""),
-        field:'name',
-        search: function (value) {
-            autocompleter.vm.term(value.toLowerCase())
-        },
-        filter: function (item) {
-            return autocompleter.vm.term()
-                   && item[autocompleter.vm.field].toLowerCase().indexOf(autocompleter.vm.term()) > -1
-        }
-    }
-    autocompleter.view = function (ctrl) {
-        return [
-            m("div", [
-                m("input", {oninput: m.withAttr("value", autocompleter.vm.search)})
-            ]),
-            ctrl.data().filter(autocompleter.vm.filter).map(function (item) {
-                return m("div", {onclick: ctrl.binds.bind(this, item)}, item[ctrl.field]);
-            })
-        ];
-    }
-    return autocompleter
-}
