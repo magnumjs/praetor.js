9e89317147fb48042dd9c7f3a2ab7e400117aef8
mglazer <michael.glazer@contractor.cengage.com>
2015-03-17 16:16:38 -0400

    responsive welcome page

--- src/scripts/modules/welcome/app.js
+++ src/scripts/modules/welcome/app.js
@@ -13,7 +13,12 @@ var app = function() {
                     'We will walk through some simple examples to demonstrate the way you can create and recall procedures'),
                   m('p', 'Check out the tools in the menu to the left.'),
                   m('a[href="http://goessner.net/articles/JsonPath/"]',
-                    'JSONPath reference to create queries.')
+                    'JSONPath reference to create queries.'),
+                  m('.row',{style:'padding-top:20px'},[
+                      m('.step',m('h1','1. Define'),m('code','p.proc("getTopDramaMovies","movies","byDrama")')),
+                      m('.step',m('h1','2. Inflate'),m('code','p({stores:{movies:data},queries:{"byDrama":{store:"movies",query:"$..movies[?(@.genres.indexOf(PARM)>-1)]"}}}) // allows the store to be used elsewhere by other praetors')),
+                      m('.step',m('h1','3. Run'),m('code','p.proc({name:"getTopDramaMovies", parms:{PARM:"Drama"}})[0].byDrama.length'))
+                  ])
               ]
             )
         ])
